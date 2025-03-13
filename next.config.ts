4d119c4b56c19edd689103293a900fb16b0c6379
Eric allen <eric@ericrallen.dev>
2025-03-13 16:00:15 -0400

    fix: add gh pages assetPrefix

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
   basePath: "/lost-signal",
+  assetPrefix: "/lost-signal",
   experimental: {
     viewTransition: true,
   },
