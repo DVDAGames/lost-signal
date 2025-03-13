617b8b1c6a0172ce3a6e6c64e42266bcabb51076
Eric allen <eric@ericrallen.dev>
2025-03-13 15:55:43 -0400

    fix: add gh pages basePath:

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  trailingSlash: true,
+  basePath: "https://dvdagames.github.io/lost-signal/",
   experimental: {
     viewTransition: true,
   },
